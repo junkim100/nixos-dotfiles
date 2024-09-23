febdd29dbdc033ea21e685cad10ce910c0634b7e
junkim100 <junkim100@gmail.com>
2024-09-23 13:17:32 +0900

    Added credential helper for git

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -5,6 +5,10 @@
     enable = true;
     userName = "junkim100";
     userEmail = "junkim100@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      credential.helper = "store";
+    };
   };
 }
 
